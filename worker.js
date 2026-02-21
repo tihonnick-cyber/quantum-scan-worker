ea1a7d3218613aa8ec7fedb7d8be63bdd675803c
tihonnick-cyber <tihon.nick@yahoo.com>
2026-02-20 22:29:40 -0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -33,7 +33,7 @@ async function saveAlert(ticker, price, change, volume) {
     await pool.query(
       `
       INSERT INTO alerts (ticker, price, change_percent, volume)
-      VALUES ($1, $2, $3, $4)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20)
       ON CONFLICT DO NOTHING
       `,
       [ticker, price, change, volume]
